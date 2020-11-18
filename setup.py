ec106bc50d7b20b6dfd33495ca3f9a1f3efcd7ab
Víctor Vázquez <victorvazrod@ugr.es>
2020-11-18 19:26:03 +0100

    Automate package publishing on release (#4)

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import pathlib
+
 from setuptools import setup, find_packages
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='shipyard-cli',
     version='0.1.0',
+    description='Command-line client for the Shipyard system',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/varrrro/shipyard-cli',
     author='Víctor Vázquez',
-    author_email='victorvazrod@gmail.com',
-    packages=find_packages(),
+    author_email='victorvazrod@ugr.es',
+    license='MIT',
+    packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     install_requires=[
         'click',
@@ -18,5 +32,5 @@ setup(
         [console_scripts]
         shipyard=shipyard_cli.main:cli
     ''',
-    python_requires='>=3.8'
+    python_requires='>=3.6'
 )
